3456875f6f43fd87d7b5fa05fd82d2adca0e3b54
Askay <askary18@hotmail.com>
2020-06-07 21:23:23 -0500

    Actulización de nombre de campos y funciones para la validación en el archivo register.j

--- views/mod_register.php
+++ views/mod_register.php
@@ -13,18 +13,11 @@
 
           <div class="row align-items-center">
             
-  <input name="validname" id="validname" type="hidden" value="0" />
-<input name="validmiddle_name" id="validmiddle_name" type="hidden" value="0" /> 
-<input name="validlast_name" id="validlast_name" type="hidden" value="0" />
-<input name="validFull_Name" id="validFull_Name" type="hidden" value="0" />
-<input name="validYearBirthday"  id="validYearBirthday" type="hidden" value="0" />    
-<input name="validMonthBirthday"  id="validMonthBirthday" type="hidden" value="0" />    
-<input name="validDayBirthday"  id="validDayBirthday" type="hidden" value="0" /> 
-<input name="valid_email"  id="valid_email" type="hidden" value="0" />             
-<input name="validPassword"  id="validPassword" type="hidden" value="0" /> 
-<input name="validPassword2"  id="validPassword2" type="hidden" value="0" />
-<input name="validMachPassword"  id="validMachPassword" type="hidden" value="0" />
-<input name="validCellPhone"  id="validCellPhone" type="hidden" value="0" />
+  <input name="valid_name" id="valid_name" type="hidden" value="0" />
+<input name="valid_consecutive_number" id="valid_consecutive_number" type="hidden" value="0" /> 
+<input name="valid_number_of_categories" id="valid_number_of_categories" type="hidden" value="0" />
+<input name="valid_category" id="valid_category" type="hidden" value="0" />
+
 
 
             <!-- .page-section -->
@@ -35,7 +28,6 @@
               <div id="base-style" class="card">
                 <!-- .card-body -->
                 <div class="card-body">
-	                <div class="text-center"  id="div_message_ok"></div>
 
                   <!-- .form -->
                   <form id="form_div">
@@ -47,38 +39,38 @@
                       <!--1-->
                       <div class="form-group">
                         <div class="textForm">
-                         <label for="name"> Nombre&nbsp;<i data-placement="bottom"  data-toggle="tooltip" title="Escribir  nombre(s)" class="fa fa-question-circle"></i></label>
-                          <span class="message" id="name_message"></span>
+                         <label for="name"> Nombre&nbsp;<i data-placement="bottom"  data-toggle="tooltip" title="Escribir  nombre de pelicula" class="fa fa-question-circle"></i></label>
+                          <span class="message" id="name_message">asasas</span>
                        </div>
                         <div class="input-group">
-                        <input type="text" required placeholder="Ingrese su Nombre(s)" id="name" name="name" class="form-control" onChange="Vname();">    
+                        <input type="text" required placeholder="Ingrese nombre de película" id="name" name="name" class="form-control" onChange="Vname();">    
                         <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
                       </div>         
                       <!--2-->
                       <div class="row">
                         <div class="col-md-6">
                               <div class="form-group">
-                                <div class="textForm"><label for="middle_name"> Número consecutivo<i data-placement="bottom"  data-toggle="tooltip" title="Escribir  ap. paterno" class="fa fa-question-circle"></i>&nbsp;</label><span class="message" id="middle_name_message"></span></div>
+                                <div class="textForm"><label for="consecutive_number"> Número consecutivo<i data-placement="bottom"  data-toggle="tooltip" title="Escribir  ap. paterno" class="fa fa-question-circle"></i>&nbsp;</label><span class="message" id="consecutive_number_message"></span></div>
                                 <div class="input-group">
-                                 <input type="text" required placeholder="Ingrese su Apellido Paterno" id="middle_name" name="middle_name" class="form-control" onChange="VMiddle_name();" >   
+                                 <input type="text" required placeholder="Número consecutivo" id="consecutive_number" name="consecutive_number" class="form-control" onChange="Vconsecutive_number();" >   
                                 <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
                               </div>            
                         </div>
                          <div class="col-md-6">
                               <div class="form-group">
                                 <div class="textForm">
-                                  <label for="last_name">Número de categoria <i data-placement="bottom"  data-toggle="tooltip" title="Escribir  ap. materno" class="fa fa-question-circle"></i>&nbsp;</label><span class="message" id="last_name_message"></span></div>
+                                  <label for="number_of_categories">Número de categoría <i data-placement="bottom"  data-toggle="tooltip" title="Escribir número de categoría" class="fa fa-question-circle"></i>&nbsp;</label><span class="message" id="number_of_categories_message"></span></div>
                                 <div class="input-group">
-                                  <input type="text" required placeholder="Ingrese su Apellido  Materno" id="last_name" name="last_name" class="form-control" onChange="Vlast_name();">  
+                                  <input type="text" required placeholder="Número de categoría" id="number_of_categories" name="number_of_categories" class="form-control" onChange="Vnumber_of_categories();">  
                                  <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
                               </div>             
                          </div>
                     </div>                      
 
         <div class="form-group">
-            <div class="textForm"><label for="term" class="txt_form">Categoria</label><span class="messageAlert" id="terms_and_conditions_message"></span></div>
+            <div class="textForm"><label for="term" class="txt_form">Categoría</label><span class="messageAlert" id="category_message"></span></div>
             <div class="input-group">
-             <select class="form-control"  name="terms_and_conditions" id="terms_and_conditions" onchange="Vterms_and_conditions();" >
+             <select class="form-control"  name="category" id="category" onchange="Vcategory();" >
               <option value="0" selected>Seleccione una opción</option>
               <option value="1" >Acepto</option>
     
@@ -92,9 +84,10 @@
 					<div id="error_msg" class="alert alert-danger alert-dismissable">
 						<div  class="text-center"><strong >Uups!</strong> Verifique su información.</div>
 					</div>
+					<div class="text-center"  id="div_message_ok"></div>					
 					<div class="div_btn_register">
 					<div id="loading_submit" style="display:none;"><img src='<?= $url_site;?>assets/images/ajax-loader.gif'/></div>
-						<button id="btn_save" name="btn_save" type="button" class="btn btn-primary btn-lg btn-block">Guardar</button>
+					<div id="div_btn_save"><button id="btn_save" name="btn_save" type="button" class="btn btn-primary btn-lg btn-block">Guardar</button></div>
 					<div class="response-holder"></div>
 					</div><div class="height_space"></div> 
 				</div>   
@@ -127,5 +120,5 @@
           </div><!-- /.row -->
         </div><!-- /.container -->
       </section><!-- /feature -->
-<script src="<?= $url_paht;?>assets/javascript/register_covid_patient.js"></script>
-<link href="<?= $url_paht;?>assets/stylesheets/register_covid.css" rel="stylesheet" type="text/css" /> 
+<script src="<?= $url_paht;?>assets/javascript/register.js"></script>
+<link href="<?= $url_paht;?>assets/stylesheets/register.css" rel="stylesheet" type="text/css" /> 
