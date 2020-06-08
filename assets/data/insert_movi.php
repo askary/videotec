4b1a9b44589bec3493a8e3647cb2d4579b5c65e9
Askay <askary18@hotmail.com>
2020-06-08 10:53:33 -0500

    Ya inserta el registro, funcionalidad terminada

--- assets/data/insert_movi.php
+++ assets/data/insert_movi.php
@@ -1,12 +1,16 @@
 <?php
+require_once("../../lib/movieDAO.php");
 
 
-
-	$name = $_POST['name'];
+	$name =utf8_decode($_POST['name']);
 	$consecutive_number = $_POST['consecutive_number'];
 	$number_of_categories = $_POST['number_of_categories'];
 	$category = $_POST['category'];		
-echo 'valor: name:'.$name.", consecutive_number:".$consecutive_number.", number_of_categories:".$number_of_categories.", category:".$category;
+//echo 'valor: name:'.$name.", consecutive_number:".$consecutive_number.", number_of_categories:".$number_of_categories.", category:".$category;
+$movieDAO = new MovieDAO(); 
+	$movieDAO->insert_movie($category,$name,$consecutive_number ,$number_of_categories);
+
+$movieDAO->__destruct();
 ?>
 
 
